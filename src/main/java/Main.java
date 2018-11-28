5a0543d6792dc485acc99f1284d9410a970c9c9c
Céline <celine.merand@reseau.eseo.fr>
2018-11-28 16:53:44 +0100

    Corrections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,11 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.logging.Level;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 /**
  * @author Céline MERAND
@@ -19,20 +20,24 @@ import java.util.concurrent.ConcurrentNavigableMap;
 
 public class Main {
 
+    private Main() {
+        throw new UnsupportedOperationException("Empty private constructor");
+    }
+
     public static void main(String[] args) {
         Connexion connexion = Connexion.getInstance();
-        System.out.println("---------------MAVEN---------------");
-        System.out.println("Version : " + connexion.getVersion());
+        LOGGER.log(Level.INFO, "---------------MAVEN---------------");
+        LOGGER.log(Level.INFO, "Version : " + connexion.getVersion());
 
         if(args.length == 1) {
             final List<Ville> villelist = readCSV(args[0]);
-            System.out.println(" Récupération de " + villelist.size() + "villes en base de données.");
+            LOGGER.log(Level.INFO, "Récupération de " + villelist.size() + "villes en base de données.");
 
             BddConnector bddConnector = new BddConnector();
             bddConnector.saveOnDatabase(villelist);
         }
         else {
-            System.out.println("Un paramètre est requis");
+            LOGGER.log(Level.INFO, "Un paramètre est requis");
             System.exit(1);
         }
     }
@@ -50,9 +55,7 @@ public class Main {
                 objects.add(new Ville(record));
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("ERREUR : " + e);
-            System.exit(1);
+            throw new UnsupportedOperationException("ERREUR " + e);
         }
         return objects;
     }
