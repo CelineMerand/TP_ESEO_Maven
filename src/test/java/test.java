aeaa51f5ab5ccbef0d93aa1d1331f2ddf3054ab7
Céline <celine.merand@reseau.eseo.fr>
2018-11-28 10:53:16 +0100

    Add test file

---
+++ src/test/java/test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author Céline MERAND
+ * Created on 28/11/2018
+ */
+public class test {
+
+    @Test
+    public void main() {
+        System.out.println("TEST");
+    }
+}
